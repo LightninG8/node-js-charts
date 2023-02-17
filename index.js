6307cf41623d42c164546994a22c6ccf1c103b58
Lightning8 <tov.kessler@yandex.ru>
2023-02-17 13:47:47 +0300

    fix

--- index.js
+++ index.js
@@ -99,10 +99,10 @@ app.get("/chart", async (req, res) => {
     fs.writeFileSync(fileName, buffer);
 
 
-    res.writeHead(304, {
-    'Content-Type': 'image/jpg',
-    'Content-Length': buffer.length,
-  });
+  //   res.writeHead(304, {
+  //   'Content-Type': 'image/jpg',
+  //   'Content-Length': buffer.length,
+  // });
 
     res.sendFile(fileName);
 
