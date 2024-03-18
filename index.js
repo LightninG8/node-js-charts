636fa09a7f2bb672a8202fea3386bd8cbc154846
LightninG8 <tov.kessler@yanedx.ru>
2024-03-18 17:37:20 +0300

    revert

--- index.js
+++ index.js
@@ -3,25 +3,8 @@ const { ChartJSNodeCanvas } = require("chartjs-node-canvas");
 const ChartDataLabels = require("chartjs-plugin-datalabels");
 const fs = require("fs");
 
-const bodyParser = require("body-parser");
-const ImageDataURI = require("image-data-uri");
-const axios = require("axios");
-const { ImgurClient } = require("imgur");
-const cors = require("cors");
-
 const app = express();
 const port = 3000;
-
-const client = new ImgurClient({
-  clientId: process.env.IMGUR_CLIENT_ID,
-  clientSecret: process.env.IMGUR_CLIENT_SECRET,
-  refreshToken: process.env.IMGUR_REFRESH_TOKEN,
-});
-
-
-app.use(cors());
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(express.static("public"));
 
 app.get("/chart", async (req, res) => {
   try {
@@ -32,7 +15,7 @@ app.get("/chart", async (req, res) => {
       .replace("[", "")
       .replace("]", "")
       .split(",")
-      .map((el) => (isNaN(+el) ? 0 : +el));
+      .map((el) => isNaN(+el) ? 0 : +el);
 
     const strategies = getStrategies(answers);
 
@@ -94,10 +77,11 @@ app.get("/chart", async (req, res) => {
         },
         plugins: {
           datalabels: {
-            anchor: "start",
-            align: "end",
-          },
-        },
+            anchor: 'start',
+            align: 'end',
+  
+          } 
+        }
       },
     };
 
@@ -109,7 +93,11 @@ app.get("/chart", async (req, res) => {
         req.query.type
       }.jpg`;
 
+
+    
+
     // fs.writeFileSync(fileName, buffer);
+
 
     // res.writeHead(304, {
     //   'Content-Type': 'image/jpg',
@@ -117,6 +105,7 @@ app.get("/chart", async (req, res) => {
     // });
 
     // res.sendFile(fileName);
+    
 
     // res.end(buffer);
 
@@ -129,6 +118,8 @@ app.get("/chart", async (req, res) => {
     // });
 
     res.end(buffer);
+
+
   } catch (e) {
     console.log(e);
   }
@@ -139,7 +130,7 @@ app.get("/max", (req, res) => {
     .replace("[", "")
     .replace("]", "")
     .split(",")
-    .map((el) => (isNaN(+el) ? 0 : +el));
+    .map((el) => isNaN(+el) ? 0 : +el);
 
   const data =
     req.query.type == "strategies"
@@ -158,47 +149,6 @@ app.get("/max", (req, res) => {
 
   res.json({ max: maximal.title });
 });
-
-app.post("/send_bot_notification", async (req, res) => {
-  try {
-    const date = new Date().getTime();
-    const filePath = `./public/${date}.jpg`;
-
-    await ImageDataURI.outputFile(req.body.imade_data, filePath);
-
-    const response = await client.upload({
-      image: fs.createReadStream(filePath),
-      type: "stream",
-    });
-
-    fs.unlink(filePath, () => {});
-
-    if (!response?.data?.link) {
-      return;
-    }
-
-    await axios
-      .post(
-        `https://chatter.salebot.pro/api/32cece2b975d1f7657e0e3f136f7f32b/message`,
-        {
-          message: req.body.message,
-          client_id: req.body.client_id,
-          attachment_type: "image",
-          attachment_url: response.data.link,
-        }
-      )
-      .then(function (response) {
-        console.log(response);
-      });
-
-    res.json({ status: 200 });
-  } catch (e) {
-    console.log(e);
-
-    res.json({ status: 400 });
-  }
-});
-
 
 app.listen(port, () => {
   console.log(`App has been started on port ${port}`);
